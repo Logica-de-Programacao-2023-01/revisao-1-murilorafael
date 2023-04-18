42180ea5d155a566a604c2ff39360b6f9828a60a
Rafael <rafaelmarliere033@gmail.com>
2023-04-18 16:04:03 -0300

    Resolução do exercício 3 de revisão

--- q3/q3.go
+++ q3/q3.go
@@ -1,6 +1,24 @@
 package q3
 
+import (
+	"fmt"
+	"sort"
+)
+
 func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	// Implemente sua solução aqui
-	return 0, 0, 0, nil
+	if len(numbers) == 0 {
+		return 0, 0, 0, fmt.Errorf("lista vazia")
+	}
+
+	sort.Ints(numbers)
+
+	menor := numbers[0]
+	maior := numbers[len(numbers)-1]
+	sum := 0
+
+	for i := 1; i < len(numbers)-1; i++ {
+		sum += numbers[i]
+	}
+
+	return menor, maior, float64(sum / (len(numbers) - 2)), nil
 }
