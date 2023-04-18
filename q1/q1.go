547c99e97a0eda95beedd38f44aed3b87da78573
Rafael Marliere de Oliveira <rafael.marliere@dinfor.uniceub>
2023-04-18 09:45:47 -0300

    Resolução do exercício 1 de revisão

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,31 @@
 package q1
 
+import "fmt"
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+
+	if currentPurchase <= 0 {
+		return 0, fmt.Errorf("valor da compra inválido")
+	}
+
+	sum := 0.0
+	for i := 0; i < len(purchaseHistory); i++ {
+		sum += purchaseHistory[i]
+	}
+
+	discount := 0.0
+
+	if sum/float64(len(purchaseHistory)) > 1000 {
+		discount = 0.2 * currentPurchase
+	} else if sum > 1000 {
+		discount = 0.1 * currentPurchase
+	} else if len(purchaseHistory) == 0 {
+		discount = 0.1 * currentPurchase
+	} else if sum <= 1000 && sum > 500 {
+		discount = 0.05 * currentPurchase
+	} else if sum <= 500 {
+		discount = 0.02 * currentPurchase
+	}
+
+	return discount, nil
 }
